2c3e72a1fce801cc0aa66acedc6360c5db1eab9b
BrunoReinicke <94152341+BrunoReinicke@users.noreply.github.com>
2025-05-24 05:30:35 -0300

    Implementação da cifragem de Hill em select.

--- Criptografia original.sql
+++ Criptografia original.sql
@@ -8,18 +8,40 @@ VW_Criptografia(id
                ,posicao
                ,pos_aux
                ,original
-               ,changed) 
-AS (SELECT ID
-        ,SENHA
+               ,changed
+               ,bloco
+               ,coprimo
+               ,matriz
+               ,reserva
+               ,auxiliar
+               ,contador
+               ,anterior
+               ,virgula
+               ,aux_bloco
+               ,bloco_atual
+               ,cont_blocos) 
+AS (SELECT AA.ID
+        ,AA.SENHA
         ,'' CRYPT
         ,'' DIV_CRYPT
         ,'' ENCRYPTED
-        ,SUBSTR(SENHA, 1, 1) CHAR_ATUAL
+        ,SUBSTR(AA.SENHA, 1, 1) CHAR_ATUAL
         ,2 POSICAO
-        ,LENGTH(SENHA) POS_AUX
-        ,'!#$%&''()*+,-./0123456789:;<=>?@abcdefghijklmnopqrstuvwxyz[]^_`ABCDEFGHIJKLMNOPQRSTUVWXYZ\' ORIGINAL 
-        ,'Üúùø÷öõôóòñðïîíìëêéèçæåäãâáàßÞÝÛÚÙØ×ÖÕÔÓÒÑÐÏÎÍÌËÊÉÈÇÆÅÄÃÂÁÀ¿¾½¼»º¹¸·¶µ´³²±°¯®­¬«ª©¨§¦¥¤£¡' CHANGED
-    FROM Usuario_A
+        ,LENGTH(AA.SENHA) POS_AUX
+        ,'!#$%&''()*+,-./0123456789:;<=>?@abcdefghijklmnopqrstuvwxyz[]^_`ABCDEFGHIJKLMNOPQRSTUVWXYZ\Б' ORIGINAL 
+        ,'Üúùø÷öõôóòñðïîíìëêéèçæåäãâáàßÝÛÚÙØ×ÖÕÔÓÒÑÐÏÎÍÌËÊÉÈÇÆÅÄÃÂÁÀ¿¾½¼»º¹¸·¶µ´³²±°¯®Д¬«ª©¨§¦¥¤£¡ГБ' CHANGED
+        ,'' BLOCO
+        ,1 COPRIMO
+        ,'18,11,12;13,14,15;2,16,17' MATRIZ
+        ,'19,12,13;14,15,16;3,17,18' RESERVA
+        ,'18,11,12;13,14,15;2,16,17' AUXILIAR
+        ,0 CONTADOR
+        ,'' ANTERIOR
+        ,0 VIRGULA
+        ,'' AUX_BLOCO
+        ,'' BLOCO_ATUAL
+        ,1 CONT_BLOCOS
+    FROM Usuario AA
     
     UNION ALL 
     
@@ -28,12 +50,12 @@ AS (SELECT ID
         
         ,CRYPT 
          || SUBSTR(CHANGED,
-                       MOD(12 * 
+                       MOD(18 * 
                           (INSTR(
                               CHANGED, 
                               SUBSTR(CHANGED, INSTR(ORIGINAL,CHAR_ATUAL), 1)
-                           ) - 1) + 14,
-                          LENGTH(CHANGED)
+                           ) - 1) + 20,
+                          LENGTH(CHANGED) - 1
                        ) + 1,
                    1)
                     
@@ -62,8 +84,9 @@ AS (SELECT ID
          END AS ENCRYPTED
          
         ,SUBSTR(SENHA, POSICAO, 1) CHAR_ATUAL
-        ,POSICAO + 1    
         
+        ,POSICAO + 1  
+         
         ,CASE
              WHEN (LENGTH(CRYPT) >= LENGTH(SENHA)) THEN 
                 POS_AUX - 1
@@ -73,11 +96,85 @@ AS (SELECT ID
          
         ,ORIGINAL
         ,CHANGED       
-      
+        
+        ,CASE   
+            WHEN (LENGTH(ENCRYPTED) = LENGTH(SENHA)) THEN   
+                CASE
+                    WHEN (REGEXP_COUNT(BLOCO, ',') = LENGTH(ENCRYPTED)) THEN
+                         CASE 
+                            WHEN MOD(LENGTH(SENHA), 3) = 0 THEN 
+                                BLOCO || ';'
+                            WHEN MOD(LENGTH(SENHA), 2) = 0 THEN
+                                BLOCO || (INSTR(CHANGED,'Б') - 1) || ';' 
+                            ELSE
+                                BLOCO || (INSTR(CHANGED,'Б') - 1) || ',' || (INSTR(CHANGED,'Б') - 1) || ';'
+                         END 
+                    WHEN (LENGTH(ENCRYPTED) = LENGTH(SENHA)) AND (BLOCO is null) THEN
+                        BLOCO || (INSTR(CHANGED, SUBSTR(ENCRYPTED, 1, 1)) - 1) || ','
+                     WHEN (LENGTH(ENCRYPTED) = LENGTH(SENHA)) AND (BLOCO is not null) THEN   
+                        BLOCO || (INSTR(CHANGED, SUBSTR(ENCRYPTED, POSICAO - ((LENGTH(SENHA) * 2) + 2), 1)) - 1) 
+                END
+            ELSE
+                BLOCO
+            END 
+         AS BLOCO
+         
+         ,COPRIMO
+         ,MATRIZ
+         ,RESERVA
+         
+         ,MATRIZ AUXILIAR
+         
+         ,CASE
+            WHEN (CONTADOR = 3) THEN
+                0
+            ELSE
+                CONTADOR
+            END 
+          AS CONTADOR
+          
+         ,ANTERIOR
+         ,REGEXP_COUNT(BLOCO,';') VIRGULA
+         
+         ,CASE 
+             WHEN (BLOCO_ATUAL IS NOT NULL) THEN
+                 CAST(CAST(REGEXP_SUBSTR(BLOCO_ATUAL, '[^,]+', 1, 1) AS INT) * CAST(REGEXP_SUBSTR(REGEXP_SUBSTR(AUXILIAR, '[^;]+', 1, 1), '[^,]+', 1, 1) AS INT) AS VARCHAR(65))
+                 || ',' || CAST(CAST(REGEXP_SUBSTR(BLOCO_ATUAL, '[^,]+', 1, 2) AS INT) * CAST(REGEXP_SUBSTR(REGEXP_SUBSTR(AUXILIAR, '[^;]+', 1, 1), '[^,]+', 1, 2) AS INT) AS VARCHAR(65))
+                 || ',' || CAST(CAST(REGEXP_SUBSTR(BLOCO_ATUAL, '[^,]+', 1, 3) AS INT) * CAST(REGEXP_SUBSTR(REGEXP_SUBSTR(AUXILIAR, '[^;]+', 1, 1), '[^,]+', 1, 3) AS INT) AS VARCHAR(65))
+                 || ';'
+                 || CAST(CAST(REGEXP_SUBSTR(BLOCO_ATUAL, '[^,]+', 1, 1) AS INT) * CAST(REGEXP_SUBSTR(REGEXP_SUBSTR(AUXILIAR, '[^;]+', 1, 2), '[^,]+', 1, 1) AS INT) AS VARCHAR(65))
+                 || ',' || CAST(CAST(REGEXP_SUBSTR(BLOCO_ATUAL, '[^,]+', 1, 2) AS INT) * CAST(REGEXP_SUBSTR(REGEXP_SUBSTR(AUXILIAR, '[^;]+', 1, 2), '[^,]+', 1, 2) AS INT) AS VARCHAR(65))
+                 || ',' || CAST(CAST(REGEXP_SUBSTR(BLOCO_ATUAL, '[^,]+', 1, 3) AS INT) * CAST(REGEXP_SUBSTR(REGEXP_SUBSTR(AUXILIAR, '[^;]+', 1, 2), '[^,]+', 1, 3) AS INT) AS VARCHAR(65))
+                 || ';'
+                 || CAST(CAST(REGEXP_SUBSTR(BLOCO_ATUAL, '[^,]+', 1, 1) AS INT) * CAST(REGEXP_SUBSTR(REGEXP_SUBSTR(AUXILIAR, '[^;]+', 1, 3), '[^,]+', 1, 1) AS INT) AS VARCHAR(65))
+                 || ',' || CAST(CAST(REGEXP_SUBSTR(BLOCO_ATUAL, '[^,]+', 1, 2) AS INT) * CAST(REGEXP_SUBSTR(REGEXP_SUBSTR(AUXILIAR, '[^;]+', 1, 3), '[^,]+', 1, 2) AS INT) AS VARCHAR(65))
+                 || ',' || CAST(CAST(REGEXP_SUBSTR(BLOCO_ATUAL, '[^,]+', 1, 3) AS INT) * CAST(REGEXP_SUBSTR(REGEXP_SUBSTR(AUXILIAR, '[^;]+', 1, 3), '[^,]+', 1, 3) AS INT) AS VARCHAR(65))
+             END
+          AS AUX_BLOCO        
+         
+         ,CASE 
+             WHEN (BLOCO_ATUAL is not null) THEN
+                 SUBSTR(BLOCO_ATUAL, 1, INSTR(BLOCO_ATUAL, ';') - 1)
+             ELSE
+                SUBSTR(BLOCO, 1, INSTR(BLOCO, ';') - 1)
+             END
+          AS BLOCO_ATUAL   
+          
+          ,COALESCE(REGEXP_COUNT(BLOCO,';'),0) + COALESCE(REGEXP_COUNT(BLOCO,','),0)  CONT_BLOCOS
+         
     FROM VW_Criptografia
-    WHERE POSICAO <= (LENGTH(SENHA) * 2) + 2
+    WHERE POSICAO <= ((LENGTH(SENHA) * 3) + 3 + CONT_BLOCOS)
 )
 SELECT ENCRYPTED
+    ,BLOCO_ATUAL
+    ,BLOCO
+    ,AUX_BLOCO
+    ,REGEXP_COUNT(BLOCO, ',') virgula
+    ,POSICAO
+    ,MATRIZ
+    ,RESERVA
+    ,AUXILIAR
+    ,ANTERIOR
 FROM VW_CRIPTOGRAFIA
-WHERE LENGTH(ENCRYPTED) = LENGTH(SENHA)
+--WHERE LENGTH(ENCRYPTED) = LENGTH(SENHA)
 ORDER BY ID, POSICAO
